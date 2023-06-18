a6b80d1e9bb657673bcc4565ecbac9c8657732de
Nithya Radhakrishnan <nithya@RKs-MacAir-M2.local>
2023-06-18 12:46:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 
 function App() {
+
+    const [date,setDate] = useState('');
+    const [age,setAge] = useState('');
+
+    const change = (e) => {     
+        setDate(e.target.value);
+    }
+
+    const calculate = () => {
+        
+        var x = 2023 - parseInt(date.slice(0,4));
+        setAge(x);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 style={{textAlign:'center'}}>Age Calculator</h1><br/>
+        <b><p style={{textAlign:'center'}}>Enter your date of birth</p></b>
+        <center><input type='date' className='form-control' style={{maxWidth:300}} onChange={change}></input></center><br/>
+        <center><button type='submit' className='btn btn-primary' onClick={calculate}>Calculate Age</button></center><br/>
+        <center><p>You are {age} years old.</p></center>
     </div>
   );
 }
